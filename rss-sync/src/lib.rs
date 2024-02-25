f12cef521369804228fbd85ae865eb0b565d5fdc
eramoss <edulramos@outlook.com>
2024-02-25 20:25:11 -0300

    tests sync in mock

--- rss-sync/src/lib.rs
+++ rss-sync/src/lib.rs
@@ -1,11 +1,15 @@
-use std::{fs::File, io::BufReader};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
-use rss::Channel;
+use rss::{Channel, Item};
 
-struct RssSync {
+pub struct RssSync {
     origin: Origin,
-    channel: Channel,
-    last_updated: String,
+    pub channel: Channel,
+    pub last_updated: String,
+    pub items: Vec<Item>,
 }
 
 #[derive(PartialEq, Debug)]
@@ -17,47 +21,57 @@ enum Origin {
 impl RssSync {
     pub async fn from_url(url: &str) -> Result<RssSync, Box<dyn std::error::Error>> {
         let content = reqwest::get(url).await?.bytes().await?;
-
-        let channel = Channel::read_from(&content[..])?;
-        let last_updated = channel.last_build_date().unwrap().to_string();
-        Ok(RssSync {
-            origin: Origin::Url(url.to_string()),
-            channel,
-            last_updated,
-        })
+        Self::build(&content[..], Origin::Url(url.to_string()))
     }
 
     pub fn from_file(filepath: &str) -> Result<RssSync, Box<dyn std::error::Error>> {
         let file = File::open(filepath).unwrap();
-        let channel = Channel::read_from(BufReader::new(file)).unwrap();
-        let last_updated = channel.last_build_date().unwrap().to_string();
-        Ok(RssSync {
-            origin: Origin::File(filepath.to_string()),
-            channel,
-            last_updated,
-        })
+        Self::build(BufReader::new(file), Origin::File(filepath.to_string()))
     }
 
-    pub async fn sync_channel(&self) -> Result<(), Box<dyn std::error::Error>> {
+    pub async fn sync_channel(&mut self) -> Result<(), Box<dyn std::error::Error>> {
         match &self.origin {
             Origin::Url(url) => {
                 let content = reqwest::get(url).await?.bytes().await?;
-                let channel = Channel::read_from(&content[..])?;
-                let last_updated = channel.last_build_date().unwrap().to_string();
-                if last_updated != self.last_updated {
-                    println!("Channel updated");
-                }
+                let new_channel = Channel::read_from(&content[..])?;
+                self.update(new_channel)?;
             }
             Origin::File(filepath) => {
                 let file = File::open(filepath).unwrap();
-                let channel = Channel::read_from(BufReader::new(file)).unwrap();
-                let last_updated = channel.last_build_date().unwrap().to_string();
-                if last_updated != self.last_updated {
-                    println!("Channel updated");
-                }
+                let new_channel = Channel::read_from(BufReader::new(file))?;
+                self.update(new_channel)?;
             }
         }
         Ok(())
+    }
+
+    fn update(&mut self, new_channel: Channel) -> Result<(), Box<dyn std::error::Error>> {
+        let last_updated = new_channel.last_build_date().unwrap().to_string();
+        Ok(if last_updated != self.last_updated {
+            self.sync_items(new_channel.items())?;
+            self.last_updated = last_updated;
+        })
+    }
+
+    fn sync_items(&mut self, new_items: &[Item]) -> Result<(), Box<dyn std::error::Error>> {
+        let mut new_items = new_items.to_vec();
+        new_items.retain(|new_item| !self.items.iter().any(|item| item.link() == new_item.link()));
+        Ok(self.items.extend(new_items))
+    }
+
+    fn build<R>(content: R, origin: Origin) -> Result<RssSync, Box<dyn std::error::Error>>
+    where
+        R: BufRead,
+    {
+        let channel = Channel::read_from(content)?;
+        let last_updated = channel.last_build_date().unwrap().to_string();
+        let items = channel.items();
+        Ok(RssSync {
+            origin,
+            channel: channel.clone(),
+            last_updated,
+            items: items.to_vec(),
+        })
     }
 }
 
@@ -68,16 +82,9 @@ mod tests {
 
     #[tokio::test]
     async fn test_rss_sync_from_url() {
-        let mut _m = mockito::Server::new();
-        _m.mock("GET", "/")
-            .with_status(200)
-            .with_header("content-type", "application/rss+xml")
-            .with_body(_RSS)
-            .create();
+        let (_m, rss_sync) = _build_from_mock().await;
         let addr = _m.host_with_port();
-        let rss_sync = RssSync::from_url(format!("http://{}", addr).as_str())
-            .await
-            .unwrap();
+
         assert_eq!(rss_sync.origin, Origin::Url(format!("http://{}", addr)));
         assert_eq!(rss_sync.last_updated, _RSS_LAST_UPDATED);
     }
@@ -93,6 +100,37 @@ mod tests {
             Origin::File(file.path().to_str().unwrap().to_string())
         );
         assert_eq!(rss_sync.last_updated, _RSS_LAST_UPDATED);
+    }
+
+    #[tokio::test]
+    async fn test_sync_channel() {
+        let (mut _m, mut rss_sync) = _build_from_mock().await;
+        let _m = _m
+            .mock("GET", "/")
+            .with_status(200)
+            .with_header("content-type", "application/rss+xml")
+            .with_body(_RSS_UPDATED)
+            .create();
+
+        rss_sync.sync_channel().await.unwrap();
+
+        assert_eq!(rss_sync.last_updated, _RSS_UPDATED_LAST_UPDATED);
+    }
+
+    async fn _build_from_mock() -> (mockito::ServerGuard, RssSync) {
+        let mut _m = mockito::Server::new();
+        _m.mock("GET", "/")
+            .with_status(200)
+            .with_header("content-type", "application/rss+xml")
+            .with_body(_RSS)
+            .create();
+        let addr = _m.host_with_port();
+
+        let rss_sync = RssSync::from_url(format!("http://{}", addr).as_str())
+            .await
+            .unwrap();
+
+        (_m, rss_sync)
     }
 
     const _RSS_LAST_UPDATED: &'static str = "Mon, 06 Sep 2010 00:01:00 +0000";
@@ -115,6 +153,36 @@ mod tests {
                     <link>http://example.com/item2</link>
                     <description>Item 2 description</description>
                     <pubDate>Mon, 06 Sep 2010 16:20:00 +0000</pubDate>
+                </item>
+            </channel>
+        </rss>
+        "#;
+    const _RSS_UPDATED_LAST_UPDATED: &'static str = "Mon, 07 Sep 2010 00:20:00 +0000";
+    const _RSS_UPDATED: &'static str = r#"
+        <?xml version="1.0" encoding="UTF-8"?>
+        <rss version="2.0">
+            <channel>
+                <title>Sample Feed</title>
+                <link>http://example.com/</link>
+                <description>Sample feed</description>
+                <lastBuildDate>Mon, 07 Sep 2010 00:20:00 +0000</lastBuildDate>
+                <item>
+                    <title>Item 1</title>
+                    <link>http://example.com/item1</link>
+                    <description>Item 1 description</description>
+                    <pubDate>Mon, 06 Sep 2010 16:20:00 +0000</pubDate>
+                </item>
+                <item>
+                    <title>Item 2</title>
+                    <link>http://example.com/item2</link>
+                    <description>Item 2 description</description>
+                    <pubDate>Mon, 06 Sep 2010 16:20:00 +0000</pubDate>
+                </item>
+                <item>
+                    <title>Item 3</title>
+                    <link>http://example.com/item3</link>
+                    <description>Item 3 description</description>
+                    <pubDate>Mon, 07 Sep 2010 16:20:00 +0000</pubDate>
                 </item>
             </channel>
         </rss>
