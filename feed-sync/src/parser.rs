a966d4e7c33fc24d04e6a4c30a0a76bd818afd81
eramoss <edulramos@outlook.com>
2024-02-27 19:15:22 -0300

    create function to parse entry from a sql row

--- feed-sync/src/parser.rs
+++ feed-sync/src/parser.rs
@@ -1,7 +1,11 @@
+use std::str::FromStr;
+
 use feed_rs::model::Entry;
+use rusqlite::Row;
 
 pub trait Parser {
     fn into_html(&self) -> String;
+    fn from_row(row: Row) -> Self;
 }
 
 impl Parser for Entry {
@@ -49,5 +53,46 @@ impl Parser for Entry {
             ));
         }
         html
+    }
+    fn from_row(row: Row) -> Self {
+        Self {
+            id: row.get("id").unwrap_or_default(),
+            title: Some(feed_rs::model::Text {
+                content: row.get("title").unwrap_or_default(),
+                content_type: mime::Mime::from_str("*/*").unwrap(),
+                src: None,
+            }),
+            summary: Some(feed_rs::model::Text {
+                content: row.get("summary").unwrap_or_default(),
+                content_type: mime::Mime::from_str("*/*").unwrap(),
+                src: None,
+            }),
+            content: Some(feed_rs::model::Content {
+                body: Some(row.get("content").unwrap_or_default()),
+                content_type: mime::Mime::from_str("*/*").unwrap(),
+                length: None,
+                src: None,
+            }),
+            authors: vec![feed_rs::model::Person {
+                email: None,
+                name: row.get("author").unwrap_or_default(),
+                uri: None,
+            }],
+            categories: vec![feed_rs::model::Category {
+                label: None,
+                scheme: None,
+                term: row.get("categories").unwrap_or_default(),
+            }],
+            links: vec![feed_rs::model::Link {
+                href: row.get("link").unwrap_or_default(),
+                length: None,
+                rel: None,
+                title: None,
+                media_type: None,
+                href_lang: None,
+            }],
+            language: Some(row.get("language").unwrap_or_default()),
+            ..Default::default()
+        }
     }
 }
