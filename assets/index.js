c4de41f4d37cb8805f1e41b19646e5e377744b39
eramoss <edulramos@outlook.com>
2024-02-28 15:07:19 -0300

    added header html to front-end handle add feed

--- assets/index.js
+++ assets/index.js
@@ -30,3 +30,57 @@ document.addEventListener('DOMContentLoaded', function () {
   });
   fetchNextContent(null);
 });
+function sendFeed(url) {
+  fetch('/add-feed', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ url: url })
+  })
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    displayErrorMessageInsideInput(document.getElementById('add-feed-input'), 'Feed not found');
+  })
+  .catch(error => {
+    // Handle error
+    console.error('Error:', error);
+    displayErrorMessage('There was an error processing your request.');
+  });
+}
+
+function displayErrorMessageInsideInput(input, message) {
+  const errorMessage = input.nextElementSibling;
+  if (errorMessage && errorMessage.classList.contains('error-message')) {
+    errorMessage.textContent = message;
+  } else {
+    const errorDiv = document.createElement('div');
+    errorDiv.classList.add('error-message');
+    errorDiv.textContent = message;
+    input.parentNode.insertBefore(errorDiv, input.nextSibling);
+    setTimeout(() => {
+      errorDiv.remove();
+    }, 2000); // Remove error message after 2 seconds
+  }
+}
+
+// Event listener for Enter key press
+document.getElementById('add-feed-input').addEventListener('keypress', function(event) {
+  if (event.key === 'Enter') {
+    const url = event.target.value.trim();
+    const input = event.target;
+    if (url !== '') {
+      sendFeed(url);
+      event.target.value = '';
+      const errorMessage = input.nextElementSibling;
+      if (errorMessage && errorMessage.classList.contains('error-message')) {
+        errorMessage.parentNode.removeChild(errorMessage);
+      }
+    } else {
+      console.error('URL cannot be empty');
+      displayErrorMessageInsideInput(input, 'URL cannot be empty');
+    }
+  }
+});
