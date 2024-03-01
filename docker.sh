98a86ad5ed1f90f20d00d7b37376095ba2bffb89
eramoss <edulramos@outlook.com>
2024-03-01 13:46:24 -0300

    removed useless script

--- docker.sh
+++
@@ -1,4 +0,0 @@
-# docker buildx build -t rustlefeed .   // Only first time
-# mkdir -p ./db
-docker volume create rustlefeed_db
-docker run -d -p 8000:8000  -v rustlefeed_db:/app/db/ rustlefeed:latest
